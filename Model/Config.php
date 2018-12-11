11d157b785300156544eed74ffa0206169a0461f
Ihor Vansach <ihor@magefan.com>
2018-12-11 10:32:16 +0200

    Added additional block lazy settings

---
+++ Model/Config.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Copyright © Ihor Vansach (ihor@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace Magefan\LazyLoad\Model;
+
+use Magento\Framework\App\Action\Action;
+
+/**
+ * Lazy load config
+ */
+class Config extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    const XML_PATH_ENABLED = 'mflazyzoad/general/enabled';
+    const XML_PATH_AMP_ENABLED = 'pramp/general/enabled';
+    const XML_PATH_LAZY_BLOCKS = 'mflazyzoad/general/lazy_blocks';
+
+    /**
+     * @var array
+     */
+    protected $blocks;
+
+    /**
+     * @var bool
+     */
+    protected $enabled;
+
+    /**
+     * Retrieve store config value
+     * @param  string $path
+     * @return mixed
+     */
+    public function getConfig($path)
+    {
+        return $this->scopeConfig->getValue(
+            $path,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * Retrieve alloved blocks info
+     * @return array
+     */
+    public function getBlocks()
+    {
+        if (null === $this->blocks) {
+            $blocks = $this->getConfig(self::XML_PATH_LAZY_BLOCKS);
+
+            $blocks = str_replace(["\r\n", "\n'\r", "\n"], "\r", $blocks);
+            $blocks = explode("\r", $blocks);
+            $this->blocks = [];
+            foreach ($blocks as $block) {
+                if ($block = trim($block)) {
+                    $this->blocks[] = $block;
+                }
+            }
+        }
+
+        return $this->blocks;
+    }
+
+    /**
+     * Retrieve true if enabled
+     * @return int
+     */
+    public function getEnabled()
+    {
+        if (null === $this->enabled) {
+            $this->enabled = $this->getConfig(self::XML_PATH_ENABLED);
+
+            /* check if Plumrocket AMP enabled */
+            if ($this->enabled) {
+                $isAmpRequest = $this->getConfig(self::XML_PATH_AMP_ENABLED);
+                if ($isAmpRequest) {
+                    /* We know that using objectManager is not a not a good practice,
+                    but if Plumrocket_AMP is not installed on your magento instance
+                    you'll get error during di:compile */
+                    $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+                    $isAmpRequest = $objectManager->get('\Plumrocket\Amp\Helper\Data')
+                        ->isAmpRequest();
+                }
+                $this->enabled = !$isAmpRequest;
+            }
+        }
+
+        return $this->enabled;
+    }
+}
--- Plugin/BlockPlugin.php
+++ Plugin/BlockPlugin.php
@@ -13,11 +13,14 @@ namespace Magefan\LazyLoad\Plugin;
  */
 class BlockPlugin
 {
+    const LAZY_TAG = '<!-- MAGEFAN_LAZY_LOAD -->';
+
     /**
      * Request
      * @var \Magento\Framework\App\RequestInterface
      */
     protected $request;
+
     /**
      * Core store config
      *
@@ -30,17 +33,31 @@ class BlockPlugin
      */
     protected $blocks;
 
+    /**
+     * Lazy store config
+     *
+     * @var \Magefan\LazyLoad\Model\Config
+     */
+    protected $config;
 
     /**
-     * @param \Magento\Framework\App\RequestInterface            $request
+     * BlockPlugin constructor
+     * @param \Magento\Framework\App\RequestInterface $request
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param null|\Magefan\LazyLoad\Model\Config $config
      */
     public function __construct(
         \Magento\Framework\App\RequestInterface $request,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        $config = null
     ) {
         $this->request = $request;
         $this->scopeConfig = $scopeConfig;
+
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $this->config = $config ?: $objectManager->get(
+            \Magefan\LazyLoad\Model\Config::class
+        );
     }
 
 
@@ -51,7 +68,7 @@ class BlockPlugin
      */
     public function afterToHtml(\Magento\Framework\View\Element\AbstractBlock $block, $html)
     {
-        if (!$this->isEnabled($block)) {
+        if (!$this->isEnabled($block, $html)) {
             return $html;
         }
 
@@ -59,80 +76,40 @@ class BlockPlugin
             $block->getViewFileUrl('Magefan_LazyLoad::images/pixel.jpg') . '" ' .
             'data-original="$2" $1 $3/>', $html);
 
-        /*
-        $i = $block->getNameInLayout(). ' ' . $block->getBlockId() . ' ' . get_class($block);
-        $html =  'start of <br/><br/>'. $i . $html;
-        */
+        $html = str_replace(self::LAZY_TAG, '', $html);
+
         return $html;
     }
 
     /**
-     * Check if lazy load is available
+     * Check if lazy load is available for block
      * @param \Magento\Framework\View\Element\AbstractBlock $block
+     * @param string $html
      * @return boolean
      */
-    protected function isEnabled($block)
+    protected function isEnabled($block, $html)
     {
         if (PHP_SAPI === 'cli' || $this->request->isXmlHttpRequest()) {
             return false;
         }
 
+        if (!$this->config->getEnabled()) {
+            return false;
+        }
+
         $blockName = $block->getBlockId() ?: $block->getNameInLayout();
-        $blocks = $this->getBlocks();
+        $blockTemplate = $block->getTemplate();
+        $blocks = $this->config->getBlocks();
 
         if (!in_array($blockName, $blocks)
             && !in_array(get_class($block), $blocks)
+            && !in_array($blockTemplate, $blocks)
+            && (false === strpos($html, self::LAZY_TAG))
         ) {
             return false;
         }
 
-        $enabled = $this->scopeConfig->getValue(
-            'mflazyzoad/general/enabled',
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        );
-
-        /* check if Plumrocket AMP enabled */
-        if ($enabled) {
-            $isAmpRequest = $this->scopeConfig->getValue(
-                'pramp/general/enabled',
-                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-            );
-            if ($isAmpRequest) {
-                /* We know that using objectManager is not a not a good practice,
-                but if Plumrocket_AMP is not installed on your magento instance
-                you'll get error during di:compile */
-                $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-                $isAmpRequest = $objectManager->get('\Plumrocket\Amp\Helper\Data')
-                    ->isAmpRequest();
-            }
-            $enabled = !$isAmpRequest;
-        }
-
-        return $enabled;
+        return true;
     }
 
-    /**
-     * Retrieve alloved blocks info
-     * @return array
-     */
-    protected function getBlocks()
-    {
-        if (null === $this->blocks) {
-            $blocks = $this->scopeConfig->getValue(
-                'mflazyzoad/general/lazy_blocks',
-                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-            );
-
-            $blocks = str_replace(["\r\n", "\n'\r", "\n"], "\r", $blocks);
-            $blocks = explode("\r", $blocks);
-            $this->blocks = [];
-            foreach ($blocks as $block) {
-                if ($block = trim($block)) {
-                    $this->blocks[] = $block;
-                }
-            }
-        }
-
-        return $this->blocks;
-    }
 }
