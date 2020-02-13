1a39a15680cc83aefc6511b84b5ec94eb003252b
Ihor Vansach <ihor@magefan.com>
2020-02-13 17:47:08 +0200

    Added ability to edit lazy load config via theme layout.xml

--- Block/Lazy.php
+++ Block/Lazy.php
@@ -31,4 +31,23 @@ class Lazy extends \Magento\Framework\View\Element\Template
 
         return '';
     }
+
+    /**
+     * Retrieve lazy load config json string
+     *
+     * @return string
+     */
+    public function getLazyLoadConfig()
+    {
+        $config = $this->getData('lazy_load_config');
+
+        if (!$config || !is_array($config)) {
+            $config = [
+                'elements_selector' => 'img,div',
+                'data_srcset' => 'originalset',
+            ];
+        }
+
+        return json_encode($config);
+    }
 }
