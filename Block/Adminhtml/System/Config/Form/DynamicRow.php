bc4d19f58a4d2405dd4bf6e7d31e3225239a43f1
DenysTsymbal <denys.tsymbal@magefan.com>
2023-06-07 17:16:27 +0300

    8739-lazy-load-webp-on-category-page-add-copyright

--- Block/Adminhtml/System/Config/Form/DynamicRow.php
+++ Block/Adminhtml/System/Config/Form/DynamicRow.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+declare(strict_types=1);
 
 namespace Magefan\LazyLoad\Block\Adminhtml\System\Config\Form;
 
